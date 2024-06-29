94a5b9fc2f00be87fed50144fc372734a7f00bc9
wj-Mcat <1435130236@qq.com>
2024-06-29 20:34:04 +0800

    add comment plugin blog

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -20,7 +20,7 @@ const config: Config = {
   deploymentBranch: "gh-pages",
   trailingSlash: false,
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'ignore',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
